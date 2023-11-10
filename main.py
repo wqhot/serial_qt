54d75dc0b524a1572c8b608b9d6a25c3bef04f09
wqhot <wqhot@outlook.com>
2023-11-10 22:31:18 +0800

    feat: 增加状态栏

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import serial
 import serial.tools.list_ports
 from PyQt5.QtWidgets import (
     QApplication,
+    QMainWindow,
     QWidget,
     QComboBox,
     QPushButton,
@@ -13,12 +14,14 @@ from PyQt5.QtWidgets import (
     QLabel,
     QHBoxLayout,
     QVBoxLayout,
+    QStatusBar,
 )
 from PyQt5.QtCore import Qt, pyqtSignal
 
 PARAMS_ROWS = 6
 PARAMS_COLS = 10
 BAUD_RATE = 115200
+
 
 class SerialData:
     def __init__(self, utime, params, msg_type, status, loop_count):
@@ -31,6 +34,8 @@ class SerialData:
 
 class SerialReaderThread(threading.Thread):
     data_received = pyqtSignal(SerialData)
+    checksum_error = pyqtSignal()
+    frame_header_error = pyqtSignal()
 
     def __init__(self, serial_port):
         super().__init__()
@@ -39,37 +44,37 @@ class SerialReaderThread(threading.Thread):
 
     def run(self):
         while self.running:
-            if (
-                self.serial_port.read() == b"\xeb"
-                and self.serial_port.read() == b"\x90"
-            ):
-                length = struct.unpack("<H", self.serial_port.read(2))[0]
-                utime = struct.unpack("<d", self.serial_port.read(8))[0]
-                params = [0.0] * (length // 4 - 3)
-                for i in range(length // 4 - 3):
-                    params[i] = struct.unpack("<f", self.serial_port.read(4))[0]
+            if self.serial_port.read() == b"\xeb":
+                if self.serial_port.read() == b"\x90":
+                    length = struct.unpack("<H", self.serial_port.read(2))[0]
+                    utime = struct.unpack("<d", self.serial_port.read(8))[0]
+                    params = [0.0] * (length // 4 - 3)
+                    for i in range(length // 4 - 3):
+                        params[i] = struct.unpack("<f", self.serial_port.read(4))[0]
 
-                msg_type = struct.unpack("<B", self.serial_port.read(1))[0]
-                status = struct.unpack("<B", self.serial_port.read(1))[0]
-                loop_count = struct.unpack("<B", self.serial_port.read(1))[0]
-                checksum = struct.unpack("<B", self.serial_port.read(1))[0]
+                    msg_type = struct.unpack("<B", self.serial_port.read(1))[0]
+                    status = struct.unpack("<B", self.serial_port.read(1))[0]
+                    loop_count = struct.unpack("<B", self.serial_port.read(1))[0]
+                    checksum = struct.unpack("<B", self.serial_port.read(1))[0]
 
-                sum = length + msg_type + status + loop_count
-                for value in params:
-                    for byte in struct.pack("<f", value):
+                    sum = length + msg_type + status + loop_count
+                    for value in params:
+                        for byte in struct.pack("<f", value):
+                            sum += byte
+
+                    for byte in struct.pack("<d", utime):
                         sum += byte
 
-                for byte in struct.pack("<d", utime):
-                    sum += byte
-
-                if sum & 0xFF == checksum:
-                    data = SerialData(utime, params, msg_type, status, loop_count)
-                    self.data_received.emit(data)
+                    if sum & 0xFF == checksum:
+                        data = SerialData(utime, params, msg_type, status, loop_count)
+                        self.data_received.emit(data)
+                    else:
+                        self.checksum_error.emit()
                 else:
-                    print("Checksum error!")
+                    self.frame_header_error.emit()
 
 
-class MainWindow(QWidget):
+class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
@@ -108,10 +113,15 @@ class MainWindow(QWidget):
         vbox = QVBoxLayout()
         vbox.addLayout(hbox)
         vbox.addLayout(layout)
-        self.setLayout(vbox)
+        widget = QWidget()
+        widget.setLayout(vbox)
+        self.setCentralWidget(widget)
 
         self.serial_port = None
         self.serial_reader_thread = None
+
+        self.status_bar = QStatusBar(self)
+        self.setStatusBar(self.status_bar)
 
         self.show()
 
@@ -134,22 +144,33 @@ class MainWindow(QWidget):
             self.serial_port = serial.Serial(port_name, BAUD_RATE, timeout=1)
             self.serial_reader_thread = SerialReaderThread(self.serial_port)
             self.serial_reader_thread.data_received.connect(self.update_line_edits)
+            self.serial_reader_thread.checksum_error.connect(self.show_checksum_error)
+            self.serial_reader_thread.frame_header_error.connect(
+                self.show_frame_header_error
+            )
             self.serial_reader_thread.start()
             self.open_serial_port_button.setText("Close Serial Port")
         except serial.SerialException as e:
             print(e)
 
     def update_line_edits(self, data: SerialData):
-        self.line_edit_widgets[0].setText(str(data.utime))
         for i, value in enumerate(data.params):
             if i >= len(self.line_edit_widgets):
                 return
-            self.line_edit_widgets[i + 1].setText(str(value))
-        if len(data.params) + 3 > len(data.params):
+            self.line_edit_widgets[i].setText(str(value))
+        if len(data.params) + 4 > len(data.params):
             return
+        self.line_edit_widgets[0 + len(data.params)].setText(str(data.utime))
         self.line_edit_widgets[1 + len(data.params)].setText(str(data.msg_type))
         self.line_edit_widgets[2 + len(data.params)].setText(str(data.status))
         self.line_edit_widgets[3 + len(data.params)].setText(str(data.loop_count))
+        self.status_bar.showMessage("Data received!")
+
+    def show_checksum_error(self):
+        self.status_bar.showMessage("Checksum error!")
+
+    def show_frame_header_error(self):
+        self.status_bar.showMessage("Frame header not found!")
 
 
 if __name__ == "__main__":
